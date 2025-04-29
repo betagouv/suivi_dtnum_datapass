fd16091fe82553f4c6fe155bf519bd85f38d867b
Apido-Hossini Consultant <apido-hossini.consultant@ad.infra.dgfip>
2025-04-29 18:26:18 +0200

    Adding proxy URLs for DataPass API calls and creating env vars for proxy_url, client_id and client_secret

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 import os
 import time
 from suivi_dtnum_updater import SuiviDtnumUpdater
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Define paths relative to the script's directory
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,13 +12,16 @@ INPUT_FILE_PATH = os.path.join(SCRIPT_DIR, "sources", "Fichier de suivi DTNUM 14
 OUTPUT_FILE_PATH = os.path.join(SCRIPT_DIR, "sources", time.strftime("%Y%m%d-%H%M%S") + "-fichier-suivi-maj.xlsx")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python update_suivi_dtnum.py <client_id> <client_secret>")
-        sys.exit(1)
+    #if len(sys.argv) != 3:
+    #    print("Usage: python update_suivi_dtnum.py <client_id> <client_secret>")
+    #    sys.exit(1)
         
     # TODO : variable d'environnement instead
-    client_id = sys.argv[1]
-    client_secret = sys.argv[2]
+    #client_id = sys.argv[1]
+    #client_secret = sys.argv[2]
+
+    client_id=os.getenv("CLIENT_ID")
+    client_secret=os.getenv("CLIENT_SECRET")
 
     updater = SuiviDtnumUpdater(client_id, client_secret)
     updater.run(INPUT_FILE_PATH, OUTPUT_FILE_PATH)
