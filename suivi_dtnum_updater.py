449239e69cd469762153eb2ad2d0360ee9e61d38
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-05-15 10:09:54 +0200

    Quotas : favor datapass content

--- suivi_dtnum_updater.py
+++ suivi_dtnum_updater.py
@@ -4,6 +4,9 @@ from datapass_row_maker import DatapassRowMaker
 from address_api_client import AddressApiClient
 
 class SuiviDtnumUpdater:
+    # We want to overwrite only these colomns from input with datapass content. The rest is overwritten only if it's empty in input.
+    MANDATORY_COLUMNS_TO_OVERWRITE = ['N° DataPass FC rattaché', 'Statut', 'Nom projet', 'Description projet', 'Destinataires des données', 'Date prévisionnelle d\'ouverture de service', 'Volumétrie', 'Date de dernière soumission', 'Quotas']
+    
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
@@ -107,16 +110,14 @@ class SuiviDtnumUpdater:
         return output_rows
 
     def merge_input_row_and_datapass_row(self, input_row, datapass_row):
-        # We want to overwrite only these colomns from input with datapass content. The rest is overwritten only if it's empty in input.
-        mandatory_columns_to_overwrite = ['Statut', 'Nom projet', 'Description projet', 'Destinataires des données', 'Date prévisionnelle d\'ouverture de service', 'Volumétrie', 'Date de dernière soumission', 'N° DataPass FC rattaché']
         # This makes a copy of the input_row without the columns we want to overwrite
-        cleaned_input_row = input_row.drop(columns=mandatory_columns_to_overwrite)
+        cleaned_input_row = input_row.drop(columns=self.MANDATORY_COLUMNS_TO_OVERWRITE)
         # This updates the cleaned_input_row empty values with the datapass_row values
         # then restore dropped columns with values from datapass_row
         # then updates the result with the initial input_row values in case there were some empty values left in mandatory columns
         # (Note : We might need to adapt the combination differently for the v1 and the v2 data)
         temp_output_row = cleaned_input_row.combine_first(datapass_row)
-        for col in mandatory_columns_to_overwrite:
+        for col in self.MANDATORY_COLUMNS_TO_OVERWRITE:
             if col in datapass_row:
                 temp_output_row[col] = datapass_row[col]        
         output_row = temp_output_row.combine_first(input_row)
